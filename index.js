0c7f71af8629628c9dd4972bccdbc654f949843b
Jamie Bryson <jamieab98@gmail.com>
2025-09-14 11:11:42 -0400

    created a way for synonyms and antonyms but I think it's wrong. I commented it out but wanted to save for potential future reference

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const testBox=document.getElementById("testing-box");
-const wordSubmit=document.getElementById("user-form");
-const wordContainer=document.getElementById("word-container");
-const url="https://api.dictionaryapi.dev/api/v2/entries/en/";
+const testBox = document.getElementById("testing-box");
+const wordSubmit = document.getElementById("user-form");
+const wordContainer = document.getElementById("word-container");
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 wordSubmit.addEventListener("submit", (event) => {
     wordContainer.textContent="";
-    const userWord=document.getElementById("user-word").value
+    const userWord = document.getElementById("user-word").value
     fetch(`${url}${userWord}`)
         .then(res => res.json())
         .then(data => {
@@ -24,6 +24,31 @@ wordSubmit.addEventListener("submit", (event) => {
                 wordContainer.append(newMeaning);
                 newMeaning.append(newListPOS, newListDef, newListExample);
             })
+            /*const synonymHeading = document.createElement("h3");
+            synonymHeading.textContent = "List of Synonyms";
+            wordContainer.append(synonymHeading);
+            const synonymList = document.createElement("ul");
+            const synonyms = data[0].meanings[1].synonyms;
+            synonyms.forEach((synonym) => {
+                const newSynonym = document.createElement("li");
+                newSynonym.textContent = synonym;
+                wordContainer.append(synonymList);
+                synonymList.append(newSynonym);
+            })
+            const antonymHeading = document.createElement("h3");
+            antonymHeading.textContent = "List of Antonyms";
+            wordContainer.append(antonymHeading);
+            const antonymList = document.createElement("ul");
+            const antonyms = data[0].meanings[1].antonyms;
+            antonyms.forEach((antonym) => {
+                const newAntonym = document.createElement("li");
+                newAntonym.textContent = antonym;
+                wordContainer.append(antonymList);
+                antonymList.append(newAntonym);
+            })*/
+        })
+        .catch(error => {
+            wordContainer.textContent = error.message;
         })
     event.preventDefault();
 })
