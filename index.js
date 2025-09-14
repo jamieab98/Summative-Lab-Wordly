b7b7bb8b0548e80ff409d4b070022701b2cfb6d2
Jamie Bryson <jamieab98@gmail.com>
2025-09-14 11:24:40 -0400

    fixed the synonom and antonym lists

--- index.js
+++ index.js
@@ -15,37 +15,19 @@ wordSubmit.addEventListener("submit", (event) => {
                 const newListPOS = document.createElement("li");
                 const newListDef = document.createElement("li");
                 const newListExample = document.createElement("li");
+                const newListSynonym = document.createElement("li");
+                const newListAntonym = document.createElement("li");
                 newListPOS.textContent = `Part of Speech: ${meaning.partOfSpeech}`;
                 newListDef.textContent = `Definition: ${meaning.definitions[0].definition}`;
                 newListExample.textContent = `Example: ${meaning.definitions[0].example}`;
                 if (meaning.definitions[0].example == undefined) {
                     newListExample.textContent = "There is no example sentence for this definition"
                 }
+                newListSynonym.textContent = `Synonym(s) of this form of the word: ${meaning.synonyms}`;
+                newListAntonym.textContent = `Antonym(s) of this form of the word: ${meaning.antonyms}`;
                 wordContainer.append(newMeaning);
-                newMeaning.append(newListPOS, newListDef, newListExample);
+                newMeaning.append(newListPOS, newListDef, newListExample, newListSynonym, newListAntonym);
             })
-            /*const synonymHeading = document.createElement("h3");
-            synonymHeading.textContent = "List of Synonyms";
-            wordContainer.append(synonymHeading);
-            const synonymList = document.createElement("ul");
-            const synonyms = data[0].meanings[1].synonyms;
-            synonyms.forEach((synonym) => {
-                const newSynonym = document.createElement("li");
-                newSynonym.textContent = synonym;
-                wordContainer.append(synonymList);
-                synonymList.append(newSynonym);
-            })
-            const antonymHeading = document.createElement("h3");
-            antonymHeading.textContent = "List of Antonyms";
-            wordContainer.append(antonymHeading);
-            const antonymList = document.createElement("ul");
-            const antonyms = data[0].meanings[1].antonyms;
-            antonyms.forEach((antonym) => {
-                const newAntonym = document.createElement("li");
-                newAntonym.textContent = antonym;
-                wordContainer.append(antonymList);
-                antonymList.append(newAntonym);
-            })*/
         })
         .catch(error => {
             wordContainer.textContent = error.message;
